8d97ce370a68d226e15ff265163fa622aad303ce
Fizza-hub902 <shanza1202@gmail.com>
2024-12-30 12:26:03 +0500

    second last commit

--- asm-file.asm
+++ asm-file.asm
@@ -1,27 +1,38 @@
-
-
 INCLUDE Irvine32.inc
-.data
-;public asmfunc
-msg db "assebly procedure end now",0
 
+.data
+msg db "Assembly procedure ended now.", 0
+msg1 db "Sum of even values: ", 0
+msg2 db "Sum of odd values: ", 0
+array SWORD -6, -3, -1, -10, 10, 30, 40, 4  ; Array of 16-bit integers
+arraySize DWORD (SIZEOF array) / 2          ; Number of elements in the array
+sumEven DWORD 0                             ; Variable for sum of even values
+sumOdd DWORD 0                              ; Variable for sum of odd values
 
 .code
-asmfunc PROC p1:DWORD, p2:DWORD
-    
-    push ebp
-    mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
-    call writedec
-    call crlf
-    ;add eax, p2 ; Add the second parameter to EAX
-    call writedec
-    call crlf
-    mov edx, offset msg
-    call writestring
-    call crlf
-    
+sum_values proc
+    xor eax, eax                ; Clear EAX (even sum)
+    xor edx, edx                ; Clear EDX (odd sum)
+    mov esi, OFFSET array       ; Load address of array into ESI
+    mov ecx, arraySize          ; Load the number of elements
+
+next_element:
+    movsx ebx, WORD PTR [esi]   ; Load current element
+    test bx, 1                  ; Check if the number is odd
+    jnz add_to_odd
+    add eax, ebx                ; Add to even sum
+    jmp skip_add
+
+add_to_odd:
+    add edx, ebx                ; Add to odd sum
+
+skip_add:
+    add esi, 2                  ; Move to next element (SWORD is 2 bytes)
+    loop next_element           ; Loop through all elements
+
+    mov sumEven, eax            ; Store the even sum
+    mov sumOdd, edx             ; Store the odd sum
     ret
-asmfunc ENDP
+sum_values endp
+
 end
--- c-file.c
+++ c-file.c
@@ -16,20 +16,25 @@ void __stdcall asmfunc(int p1 ,int p2);
 
 
 int main() {
-    system("cls");
-    int abc;
-    printf("assembly proc calling from  from C! \n");
-getch();
-
-    
-    asmfunc(3,5); //assembly proc calling
    
-   getch();
-    
-    
-    printf("back to  C! \n"); // printing in c
-    
-  
-    
+    int array[] = {-6, -3, -1, -10, 10, 30, 40, 4};  
+    int arraySize = sizeof(array) / sizeof(array[0]); // Number of elements in the array
+    int sumEven = 0; 
+    int sumOdd = 0;  
+
+   
+    for (int i = 0; i < arraySize; i++) {
+        if (array[i] % 2 == 0) {  
+            sumEven += array[i];
+        } else { 
+            sumOdd += array[i];
+        }
+    }
+
+    // Display the results
+    printf("Sum of even values: %d\n", sumEven);
+    printf("Sum of odd values: %d\n", sumOdd);
+    printf("Assembly procedure ended now.\n");
+
     return 0;
 }
